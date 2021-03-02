07930ff568c1f3d17fd852e292f9792c26da0780
Devyn Bailey <devynbailey11@yahoo.com>
2021-03-01 22:11:13 -0700

    added footer component, added react-icon package

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience";
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
     <Experience />
     <Projects />
     <Contact />
+    <Footer />
 
     </>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaGithubSquare, FaLinkedinIn } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="d-flex">
+              <p>Copyright Devyn Bailey</p>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-2 col-sm-6">
+            <div className="row">
+              <div className="col">
+                <a className="foot-nav">Home</a>
+                <br />
+                <a className="foot-nav">About Me</a>
+                <br />
+                <a className="foot-nav">Experience</a>
+              </div>
+              <div className="col">
+                <a className="foot-nav">Projects</a>
+                <br />
+                <a className="foot-nav">Contact Me</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5 col-md-5 col-sm-6 align-items-center">
+            <div className="d-flex justify-content-center">
+              <a href="https://github.com/dbailey11">
+                <FaGithubSquare />
+              </a>
+            </div>
+            <div className="d-flex justify-content-center">
+              <a href="https://www.linkedin.com/in/devyn-bailey/">
+                <FaLinkedinIn />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
